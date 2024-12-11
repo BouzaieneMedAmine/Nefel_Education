fde07873dbc56172c5fd78198a8470f2af33fc46
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-11 14:31:06 +0100

    Get On That Ride (Core)

---
+++ Programing Basics/week1/day3/core/Code Challenge Get On That Ride (Core)/Med_Amine_bouzaiene_Get On That Ride.js
@@ -0,0 +1,42 @@
+var hieght=43
+if (hieght >= 42)
+    {
+        console.log ("get on that ride", "kiddo!") 
+    }
+    else
+    {
+        console.log ("Sorry kiddo", "Maybe next year.")
+    }
+
+    
+    
+    
+
+var age=0
+if  (age >= 10 && hieght>=42 ){  
+    console.log ("get on that ride"," kiddo!");
+}
+    else
+    {
+        console.log ("Sorry kiddo. Maybe next year.")
+    }
+    
+
+if (age >= 10 || hieght>=42)
+    {
+        console.log("get on that ride", "kiddo!")
+    }
+    else
+    {
+        console.log ("Sorry kiddo", "Maybe next year.")
+    }
+    
+    
+
+
+    
+
+
+    
+
+    
