24587e430ae57ccf78b3c7cdbaa4095bfa1fb101
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-12 15:36:05 +0100

    practice

---
+++ Programing Basics/week1/day4/practice/Med_Amine_Bouzaiene_Parameters (Practice).js
@@ -0,0 +1,19 @@
+function greet(persone){
+    console.log("Good day, " + persone);
+}
+greet("amine")
+function greet2(per){ 
+    var time= new Date().toLocaleTimeString()
+console.log("Good day, " + per +" " + time);
+
+}
+greet2("Amine")
+
+function enemies(name){
+    if(name=="Count Dooku"){
+        console.log("I'm coming for you, Dooku!");
+        
+    } 
+}
+    
+enemies("Count Doku")
