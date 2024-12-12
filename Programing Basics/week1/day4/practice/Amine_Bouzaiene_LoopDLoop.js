c71bb4ab5b7945ade978ef9e8d171184b53af091
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-12 16:44:03 +0100

    loop

--- Programing Basics/week1/day4/practice/Amine_Bouzaiene_LoopDLoop.js
+++ Programing Basics/week1/day4/practice/Amine_Bouzaiene_LoopDLoop.js
@@ -5,3 +5,12 @@ for(var mile=2; mile <= 6; mile+2){
 }
 
     
+var distance=5
+var time=1
+for(mile=2; mile<=6; mile+2){
+    mile=mile+2
+    
+if(time<=1 && distance <=5.5){
+    console.log("candy");
+}
+}
