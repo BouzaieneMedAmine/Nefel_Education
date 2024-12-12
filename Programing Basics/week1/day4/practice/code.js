c216288fe4e5bfaf0c7b0a1d30dc5ed83a79731a
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-12 16:53:51 +0100

    code

---
+++ Programing Basics/week1/day4/practice/code.js
@@ -0,0 +1,7 @@
+// this var can get a value of arry, number, srting ... - what would this variable do?
+var likeCount = 0;
+// it will excute when we call it - what would be the function of this code block?
+function increaseLikes() {
+    // we add +1 to the var likeCount - what is this sort operation for?
+    likeCount = likeCount + 1;
+}
