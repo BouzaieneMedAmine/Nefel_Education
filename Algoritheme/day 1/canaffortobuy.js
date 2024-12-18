0236f02958191aa90bf1d1641d14dd26eb197626
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-18 18:37:54 +0100

    portfulio

---
+++ Algoritheme/day 1/canaffortobuy.js
@@ -0,0 +1,14 @@
+//creation of code can see if the custemers can afford to buy from shop
+//steps
+//1. ask the market how it coast the cheapest thing
+//2. ask the custemers wht hes budget
+//3. compare the budget to the cheapes thing
+//4. give acces to the custemers or not
+
+
+function canaffordtob(coast,budget)
+    if (coast=<budget) {
+        true 
+        else 
+        
+    }
