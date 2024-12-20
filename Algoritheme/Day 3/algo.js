16432f49bb8b542d900c6ef1d1a5b2096947305b
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-20 15:40:51 +0100

    core flex

---
+++ Algoritheme/Day 3/algo.js
@@ -0,0 +1,17 @@
+//make function looking for item
+//we creat for loop to search about some thing that we asking for
+//if we attend the this thing we keep num 2
+
+function arr1=["a","b","c"]{
+    var const=0
+    for (var i=0;i=<arr1.length-1 ;i++){
+        if i== c
+        return const= 2
+    }
+   
+
+}
+
+//make function looking for item
+//we creat for loop to search about some thing that we asking for
+//if we attend the this thing we keep num 2
