c3ba32170c19d985e8fe852fa158d20f9a4ed2f7
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-23 15:46:26 +0100

    loop

---
+++ Web Fundamentals/week3/day 1/practice/loop.js
@@ -0,0 +1,34 @@
+//1
+for(var i=0;i<=20;i++){
+    if(i%2 !==0){
+        console.log(i);
+    }       
+}
+
+
+//2
+for(var i=100;i<=0;i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+
+//3
+for(var i=4;i>=-3.5;i=i-1.5 ){
+    console.log(i)
+}
+
+
+//4
+var sum=0
+for(var i=0;i<=100;i++){
+sum+= i
+console.log(sum)
+}
+
+var sum=1
+for(var i=1;i<=12;i++){
+sum*= i
+console.log(sum)
+}
