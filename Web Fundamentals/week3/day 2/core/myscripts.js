c6054c8da2823d92b091c890d09db037e69e79e0
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-24 17:39:47 +0100

    Button_Cliker

---
+++ Web Fundamentals/week3/day 2/core/myscripts.js
@@ -0,0 +1,15 @@
+function login(element){
+
+    element.innerText="Logout"    
+}
+
+
+function remove(element){
+    element.remove()
+}
+
+
+function alert1(element){
+    
+    alert("This page says Ninja was liked")
+}
