8986900812c5328780d11f22f14dafa865eef427
MedAmineBouzaiene <bouzamine11@gmail.com>
2024-12-25 11:04:10 +0100

    videolab

---
+++ Algoritheme/code3.js
@@ -0,0 +1,28 @@
+ //1//we have set of number
+//2// we dvise our number by 2 and see if it even first 
+//3// if it is even we compare it if it less then 0 then it negative
+//4//when the both of condition it true we add 1 to our result
+//5//when the array empty we the result it will show 0
+
+
+var sum=0
+var nu=0
+function contEvenNegatives(nums){
+    
+    
+    for(var i=nums.lenght; i<=nums.lenght-1;i++){
+        nu=nums[length]
+        if(i%2==0 && i<0){
+            console.log(sum=sum+1);
+        }  
+        else if(nums.lenght==0){
+            console.log(0);
+            
+        }
+            
+
+    }
+    
+
+}
+contEvenNegatives=[-2, 5, 9, -8]
---
+++ Web Fundamentals/week3/day 2/practice/video-preview/script.js
@@ -0,0 +1,13 @@
+function playvideo(vid){
+    vid.play();
+
+}
+
+
+function videoPause(vid){
+    vid.pause();
+
+    
+}
+
+  
