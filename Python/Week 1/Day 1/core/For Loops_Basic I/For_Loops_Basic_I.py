7e138a9bca7552073a08496774b1acf3d780a46f
MedAmineBouzaiene <bouzamine11@gmail.com>
2025-01-08 17:28:06 +0100

    basic loop

---
+++ Python/Week 1/Day 1/core/For Loops_Basic I/For_Loops_Basic_I.py
@@ -0,0 +1,73 @@
+
+# 1
+
+i = 0 
+while i < 151:
+    print(i)
+    i=i + 1
+
+
+
+
+#  2
+
+i = 5
+while i % 5 == 0 and i < 1000 :
+    print (i)
+    i = i + 5
+
+
+
+# 3
+
+
+i = 0 
+while i<= 100:
+    print (i)
+    i=1+i
+    if  i % 10 == 0:
+        print ('Coding Dojo')
+    if i % 5 == 0:
+    
+        print ('Coding')
+
+
+
+
+# 4 
+
+
+i = 0
+sum = 0
+while i <= 500000:
+    sum= sum +i
+    i=i+1
+print(sum)
+     
+    
+
+
+
+# 5
+
+i = 2018
+while i >= 0:
+    print(i)
+    i= i-4
+
+
+
+
+# 6
+
+lownum=50
+highnum=100
+mult=3
+
+
+for element in range (lownum,highnum):
+    if element%3==0:
+     print (element)
+
+
+
