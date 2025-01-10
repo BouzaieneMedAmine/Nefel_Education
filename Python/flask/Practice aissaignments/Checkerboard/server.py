55edcbd7ef5156662643edc653cda2412cdb7243
MedAmineBouzaiene <bouzamine11@gmail.com>
2025-01-10 13:59:27 +0100

    playground

--- Python/flask/Practice aissaignments/Checkerboard/server.py
+++ Python/flask/Practice aissaignments/Checkerboard/server.py
@@ -1,10 +1,10 @@
 from flask import Flask,  render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')
-def index():
-    return render_template("index.html")
+# def index():
+#     return render_template("index.html")
 
-@app.route('/')
+@app.route('//)
 def style():
     return render_template("style.css")
 
---
+++ Python/flask/Practice aissaignments/Playground/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/play')
+def play():
+    return render_template('index.html', mult=3, color='blue')
+
+@app.route('/play/<int:x>')
+def play_x(x):
+    return render_template('index.html', mult=x, color='blue')
+
+@app.route('/play/<int:x>/<color>')
+def play_x_color(x, color):
+    return render_template('index.html', mult=x, color=color)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
