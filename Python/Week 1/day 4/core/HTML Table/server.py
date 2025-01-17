194ea277e57f78578b4697e204c4b28873b6d31a
MedAmineBouzaiene <bouzamine11@gmail.com>
2025-01-17 16:39:17 +0100

    HTML

--- Python/Week 1/day 4/core/HTML Table/server.py
+++ Python/Week 1/day 4/core/HTML Table/server.py
@@ -1,25 +1,18 @@
-class User :
-    def __init__(self,first_name,last_name,email,age) :
-        self.first_name=first_name
-        self.last_name=last_name
-        self.email=email
-        self.age=age
-        self.is_reward_member=False
-        self.gold_card_points=0
+from flask import Flask, render_template
 
-    def display_info(self):
-        print(self.first_name)
-        print(self.last_name)
-        print(self.email)
-        print(self.age)
+app = Flask(__name__)
 
-    def enroll(self):
-        if self.is_reward_member==True :
-            print('user is already a member')
-            return False
-        return True
-        self.gold_card_points=200
+# Data to be rendered
+users = [
+    {'first_name': 'Michael', 'last_name': 'Choi'},
+    {'first_name': 'John', 'last_name': 'Supsupin'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
 
-    def spend(self,amount) :
-        if self.gold_card_points >= amount :
-            self.gold_card_points=gold_card_points-amount
+@app.route('/')
+def index():
+    return render_template('index.html', users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
