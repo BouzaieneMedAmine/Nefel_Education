c01161222d7ee51baad79d59c96b103ee4fcefb6
MedAmineBouzaiene <bouzamine11@gmail.com>
2025-01-21 23:16:10 +0100

    Users

---
+++ Python/flask _mysql/crud/User CR/flask_app/controllers/users.py
@@ -0,0 +1,24 @@
+from flask import render_template, request, redirect
+from flask_app import app
+from flask_app.models.users import User
+
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+
+@app.route('/users')
+def users():
+    return render_template("users.html",users=User.get_all())
+
+
+@app.route('/user/new')
+def new():
+    return render_template("new_user.html")
+
+@app.route('/user/create',methods=['POST'])
+def create():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
