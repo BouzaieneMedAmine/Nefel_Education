fba1b9a1823c517fe6330687f9686401bc31bedc
MedAmineBouzaiene <bouzamine11@gmail.com>
2025-02-25 18:42:22 +0100

    BooksI

---
+++ MERN/Week_3/day2/core/merningBooksII/client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Nav from './components/Nav.jsx'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css' 
+
+function App() {
+  
+
+  return (
+<>
+  <Nav></Nav>
+</>
+  )
+}
+
+export default App
