7b9b9ee1908a3e4f0a6f99199ef9b8b091234c2e
BouzaieneMedAmine <bouzamine11@gmail.com>
2025-04-02 17:38:28 +0200

    st

---
+++ Java/Week2/Day3/Practice/YourOwnJavaProg/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("My name is Bouzaiene Med Amine\r\n" + //
+                        "I am 40 years old\r\n" + //
+                        "My hometown is Hammamet, TUN\r\n" + //
+                        "\r\n" + //
+                        "");
+	}
+}
